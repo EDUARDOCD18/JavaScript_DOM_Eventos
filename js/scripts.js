af6268494807e49c8a87972859959b2bbaf8bc24
eduardocd18 <javierecd@gmail.com>
2024-02-09 13:05:51 -0400

    144 - Seleccionar elemento con querySelector

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/* Seleccionar elementos de HTML */
+
+// querySelector
+const heading = document.querySelector('.header__texto h2') // retorna 0 o 1 elementos
+
+heading.textContent = "Nuevo heading" // Modifica el texto del contenido
+heading.classList.add("nueva-clase") // Agrega nueva clase
+console.log(heading);
+
+/* Se recomienda tener en cuenta BEM */
+/* JS permite manipular los elemetos de HTML */
+
+// querySelectorAll
+
+
+//getElementById

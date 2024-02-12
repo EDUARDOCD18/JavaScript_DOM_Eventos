86e5e803f3fbc21a6a7662954019a7f2d31a955a
eduardocd18 <javierecd@gmail.com>
2024-02-12 14:01:59 -0400

    146 - Crear HTML con createElement

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ const heading = document.querySelector('.header__texto h2') // retorna 0 o 1 ele
 
 heading.textContent = "Nuevo heading" // Modifica el texto del contenido
 heading.classList.add("nueva-clase") // Agrega nueva clase
-console.log(heading);
+/* console.log(heading); */
 
 /* Se recomienda tener en cuenta BEM */
 /* JS permite manipular los elemetos de HTML */
@@ -18,10 +18,10 @@ console.log(heading);
 */
 
 const enlaces = document.querySelectorAll('.navegacion a')
-console.log(enlaces);
+/* console.log(enlaces); */
 
 /* Acceder a un elemeto en específico */
-console.log(enlaces[0]);
+/* console.log(enlaces[0]); */
 
 enlaces[0].textContent = "nuevo enlace"
 
@@ -36,4 +36,26 @@ enlaces[0].classList.remove('XD-class')
 
 //getElementById
 const heading2 = document.getElementById('heading') //Poco usado en nuevas versiones de JS
-console.log(heading2);
+/* console.log(heading2); */
+
+/* 146 - Crear HTML con createElement */
+
+// - Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A') /* -- Se recomienda usar mayúscula -- */
+
+/* -- Agregar propiedades al elemento HTML -- */
+
+// - Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html'
+
+// - Agregar el texto
+nuevoEnlace.textContent = 'Tienda virtual'
+
+// - Agregar la clases
+nuevoEnlace.classList.add('navegacion__enlace')
+
+// - Agregarlo al documento
+const navegacion = document.querySelector('.navegacion')
+navegacion.appendChild(nuevoEnlace)
+
+//console.log(nuevoEnlace);
